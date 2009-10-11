76b0bc27d7ab4c41d6b74c5d3976e77621448add
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2009-10-11 22:51:14 +0600

    Initial commit to yandex_metrika.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'yandex_metrika'
+
+class Test::Unit::TestCase
+end
